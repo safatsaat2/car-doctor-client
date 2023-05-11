e2b86547b7b52b7e584b79a8894253876550ec16
safatsaat2 <sakibsafat47@gmail.com>
2023-05-11 20:52:41 +0600

    firebase install

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import LogIn from "../Pages/LogIn/LogIn";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path:'/',
-            element: <Home></Home>
-        }
-      ]
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: 'login',
+                element: <LogIn></LogIn>
+            }
+
+        ]
     },
-  ]);
+]);
 
 export default router
