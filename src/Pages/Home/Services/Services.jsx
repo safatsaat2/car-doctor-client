a94d41540b525fc0b0fb0e43a128bc084740fc7a
safatsaat2 <sakibsafat47@gmail.com>
2023-05-24 12:26:11 +0600

    search implented

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
     const [services, setServices] = useState([]);
-    const [asc, setAsc] = useState(true)
+    const [asc, setAsc] = useState(true);
+    const [search, setSearch] = useState('')
+    const searchRef = useRef(null);
+
+    const handleSearch = () =>{
+        console.log(searchRef.current.value)
+        setSearch(searchRef.current.value)
+    }
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'desc'}`)
+        fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [asc])
+    }, [asc, search])
 
     return (
         <div className="mt-4">
@@ -17,7 +24,15 @@ const Services = () => {
                 <h3 className="text-3xl text-orange-600 font-bold">Our Services</h3>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br></br> words which do not look even slightly believable. </p>
-                <button className="btn btn-primary" onClick={() => setAsc(!asc)}>{asc? "Price High to Low" : "Price Low to High"}</button>
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+                        <button onClick={handleSearch} className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+                <button className="btn btn-primary" onClick={() => setAsc(!asc)}>{asc ? "Price High to Low" : "Price Low to High"}</button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
